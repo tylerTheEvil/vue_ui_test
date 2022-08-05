f56e84ddd74c7264ea4ef0409a5a604f434a8dfd
Anton Striapunin <astriapunin@n-ix.com>
2022-08-05 14:09:07 +0300

    first commit

---
+++ src/components/EmailInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import EmailsList from './EmailsList.vue';
+
+let newEmail = ref('')
+const emails = ref([])
+
+const pushItem = (e) => {
+  e.preventDefault()
+
+  emails.value.push(newEmail.value)
+  newEmail.value = ''
+}
+</script>
+
+<template>
+  <div class="emails">
+    <EmailsList :emails='emails' />
+    <h3>New email is: {{ newEmail }}</h3>
+    <form v-on:submit="pushItem">
+      <input v-model="newEmail" placeholder="Enter email"/>
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.emails {
+  margin: 20px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+</style>
---
+++ src/components/EmailsList.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+emails: {
+    type: Array,
+    default: () => ["1","2","3"]
+  }
+})
+</script>
+
+<template>
+  <div class="emails-container">
+    <h3>Emails: </h3>
+    <p>{{ emails.join(", ") }}</p>
+  </div>
+</template>
+
+<style scoped>
+.emails-container {
+  margin-bottom: 1rem;
+}
+h3 {
+  font-size: 1.2rem;
+}
+</style>
